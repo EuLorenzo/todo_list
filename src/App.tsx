4339d2694a01153d1ee9d5c070522cc06802c032
euLorenzo <lorenzo.frinhani@gmail.com>
2024-07-05 15:47:04 -0300

    Feita parte variaveis css

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import "./global.css"
+import todo_logo from "./assets/todo_logo.svg"
+
+function App() {
+
+  return (
+    <>
+      <header className='header'>
+        <img src={todo_logo} alt="Todo logo" />
+      </header>
+      
+    </>
+  )
+}
+
+export default App
